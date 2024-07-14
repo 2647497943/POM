3137cf9e2c7d34a9bbd48f8f3806ff120aca84ee
2647497943 <2647497943@qq.com>
2024-07-14 12:46:32 +0800

    yaml配置数据、yaml文件读取

---
+++ common/yaml_config.py
@@ -0,0 +1,16 @@
+# with open('../config/environment.yaml', encoding='utf-8') as f:
+#     file = f.read()
+#     print(file)
+
+import yaml
+class GetConf():
+    def __init__(self):
+        with open('../config/environment.yaml', encoding='utf-8') as file:
+            self.env = yaml.load(file, yaml.FullLoader)
+
+    def get_username_password(self):
+        return self.env['username'], self.env['password']
+
+# if __name__ == '__main__':
+    # print(GetConf().get_username_password())
+
